1502b78e87ac1c977b911250ad5f4ed06c207dbc
lexiouellette228 <alexislouise14@icloud.com>
2025-06-23 23:13:34 -0400

    Update server.py

--- Network_Packet_Handling/server.py
+++ Network_Packet_Handling/server.py
@@ -1,23 +1,22 @@
 # server.py
-# PA1 Network Packet Handling
-# Alexis Ouelltte
+# Network Packet Handling
 
 import socket
 import struct
 
 def unpack_packet(conn, header_format):
-    #recieve the header
+    # Recieve the header
     header_size = struct.calcsize(header_format)
     header_data = conn.recv(header_size)
 
-    #check for no data and returns none if no data is found
+    # Check for no data and returns none if no data is found
     if not header_data: 
         return None
     
-    #unpacking the header based on the bytes recieved
+    # Unpacking the header based on the bytes recieved
     version, header_length, service_type, payload_length = struct.unpack(header_format, header_data)
 
-    #get the payload from the length of the payload
+    # Get the payload from the length of the payload
     payload_data = conn.recv(payload_length)
     if not payload_data:
         return None
@@ -29,20 +28,21 @@ def unpack_packet(conn, header_format):
         f"Payload: {payload_data.decode('utf-8')}"
     )
 
-    # return the string - this will be the payload
+    # Return the string - this will be the payload
     return packet_header_as_string
 
 if __name__ == '__main__':
     host = '10.128.0.2' #my VM internal IP address
     port = 12345
 
-# Fixed length header -> Version (1 byte), Header Length (1 byte), 
+# Fixed length header: Version (1 byte), Header Length (1 byte), 
 # Service Type (1 byte), Payload Length (2 bytes)
 # Specify the header format using "struct"
-# header set to 4
+
+# Header set to 4
 header_format = '!BBBH' 
 
-#create the socket for the connection
+# Create the socket for the connection
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: 
     s.bind((host, port))
     s.listen()
@@ -51,19 +51,19 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         print(f"Connected by: {addr}")
         while True:
             try:
-            # Receive and unpack packet using the unpack_packet
+            # receive and unpack packet using the unpack_packet
                 payload_string = unpack_packet(conn, header_format)
 
-            #Break after data is loaded
+            # break after data is loaded
                 if payload_string is None:
                     break
 
                 print("recieved packet: ", payload_string)
 
-            #echo back to client
+            # echo back to client
                 conn.sendall(payload_string.encode('utf-8'))
 
-            #Check for error
+            # Check for error
             except Exception as e:
                 print("Error")
                 break
